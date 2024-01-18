96073fd4ba33b048f4bd887a2b6e180c38b0b52c
yachoo2606 <janbialy@hotmail.com>
2024-01-18 11:55:57 +0100

    Provided possibility to post construct load products to each producer randomly, to increase difference between producers

---
+++ src/main/java/com/example/producer/controllers/ProductsController.java
@@ -0,0 +1,25 @@
+package com.example.producer.controllers;
+
+import com.example.producer.service.ProductsService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+    private final ProductsService productsService;
+
+    public ProductsController(ProductsService productsService) {
+        this.productsService = productsService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllProducts(){
+        return ResponseEntity.ok().body(productsService.getAllProducts());
+    }
+
+}
